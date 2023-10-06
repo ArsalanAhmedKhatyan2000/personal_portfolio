55e48cc025d2b9b462ec2da05584b4826d37feed
ArsalanAhmedKhatyan2000 <arsal.ik2008@gmail.com>
2023-10-06 20:49:00 +0500

    removed download resume and social accounts strip from drawer.

--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -22,59 +22,58 @@ class SideMenu extends StatelessWidget {
               child: SingleChildScrollView(
                 padding: const EdgeInsets.all(defaultPadding),
                 child: Column(
-                  children: [
-                    const AreaInfoText(title: "Residence", text: "Pakistan"),
-                    const AreaInfoText(title: "City", text: "Karachi"),
-                    const AreaInfoText(
-                        title: "Phone No.", text: "+92 332 0270677"),
-                    const AreaInfoText(title: "Age", text: "23"),
-                    const Skills(),
-                    const SizedBox(height: defaultPadding),
-                    const Coding(),
-                    const Knowledges(),
-                    const Divider(),
-                    const SizedBox(height: defaultPadding / 2),
-                    TextButton(
-                      onPressed: () {},
-                      child: FittedBox(
-                        child: Row(
-                          children: [
-                            Text(
-                              "Download CV",
-                              style: TextStyle(
-                                  color: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1!
-                                      .color),
-                            ),
-                            const SizedBox(width: defaultPadding / 2),
-                            SvgPicture.asset("assets/icons/download.svg"),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(top: defaultPadding),
-                      color: const Color(0xFF24242E),
-                      child: Row(
-                        children: [
-                          const Spacer(),
-                          IconButton(
-                            onPressed: () {},
-                            icon: SvgPicture.asset("assets/icons/linkedin.svg"),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: SvgPicture.asset("assets/icons/github.svg"),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: SvgPicture.asset("assets/icons/twitter.svg"),
-                          ),
-                          const Spacer(),
-                        ],
-                      ),
-                    ),
+                  children: const [
+                    AreaInfoText(title: "Residence", text: "Pakistan"),
+                    AreaInfoText(title: "City", text: "Karachi"),
+                    AreaInfoText(title: "Phone No.", text: "+92 332 0270677"),
+                    AreaInfoText(title: "Age", text: "23"),
+                    Skills(),
+                    SizedBox(height: defaultPadding),
+                    Coding(),
+                    Knowledges(),
+                    // const Divider(),
+                    // const SizedBox(height: defaultPadding / 2),
+                    // TextButton(
+                    //   onPressed: () {},
+                    //   child: FittedBox(
+                    //     child: Row(
+                    //       children: [
+                    //         Text(
+                    //           "Download CV",
+                    //           style: TextStyle(
+                    //               color: Theme.of(context)
+                    //                   .textTheme
+                    //                   .bodyText1!
+                    //                   .color),
+                    //         ),
+                    //         const SizedBox(width: defaultPadding / 2),
+                    //         SvgPicture.asset("assets/icons/download.svg"),
+                    //       ],
+                    //     ),
+                    //   ),
+                    // ),
+                    // Container(
+                    //   margin: const EdgeInsets.only(top: defaultPadding),
+                    //   color: const Color(0xFF24242E),
+                    //   child: Row(
+                    //     children: [
+                    //       const Spacer(),
+                    //       IconButton(
+                    //         onPressed: () {},
+                    //         icon: SvgPicture.asset("assets/icons/linkedin.svg"),
+                    //       ),
+                    //       IconButton(
+                    //         onPressed: () {},
+                    //         icon: SvgPicture.asset("assets/icons/github.svg"),
+                    //       ),
+                    //       IconButton(
+                    //         onPressed: () {},
+                    //         icon: SvgPicture.asset("assets/icons/twitter.svg"),
+                    //       ),
+                    //       const Spacer(),
+                    //     ],
+                    //   ),
+                    // ),
                   ],
                 ),
               ),
